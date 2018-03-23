016af8c7b3c1519e2d435308764c0bf096a3799a
Antoine <antoine-hoffmann@hotmail.com>
2018-03-23 12:47:41 +0100

    function for SGD and adam solver

--- docker_connect.sh
+++ docker_connect.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sudo docker run --rm -d --name ann -p 8888:8888 -v "$(pwd):/jupyter" zifeo/artificial-neural-networks:cpu
+sudo docker run --runtime=nvidia --rm -d --name ann -p 8888:8888 -v "$(pwd):/jupyter" zifeo/artificial-neural-networks:gpu
