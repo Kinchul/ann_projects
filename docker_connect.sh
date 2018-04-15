0ad0ce8c4759122a325bfc2ddcb32bfe3bbeb163
Kinchul <v.kindschi@gmail.com>
2018-04-15 14:13:25 +0200

    initial commit tp2

--- docker_connect.sh
+++ docker_connect.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sudo docker run --runtime=nvidia --rm -d --name ann -p 8888:8888 --pid=host -v "$(pwd):/jupyter" zifeo/artificial-neural-networks:gpu
+sudo docker run --runtime=nvidia --rm -d --name ann -p 8888:8888 -v "$(pwd):/jupyter" zifeo/artificial-neural-networks:gpu
